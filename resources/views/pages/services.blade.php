8395c3c84ef539129cc3bf1827120397227f55be
Kavie <kavie@ocision.com>
2018-05-18 16:58:35 +0800

    adding the laravel site for the first time

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'laravelsite')}}</title>
+</head>
+<body>
+
+	<h1>Services Page</h1>
+	<p>This is a paragraph</p>
+</body>
+</html>
