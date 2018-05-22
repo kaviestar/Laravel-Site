d945425c8f71dd7efdec56ce84f7cb8990cbb6ef
Kavie <kavie@ocision.com>
2018-05-22 17:33:37 +0800

    added database files and new posts

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -14,6 +14,7 @@
             <li><a href="/laravelsite/public">Home</a></li>
             <li><a href="/laravelsite/public/about">About</a></li>
             <li><a href="/laravelsite/public/services">Services</a></li>
+            <li><a href="/laravelsite/public/posts">Blog</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
